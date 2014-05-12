14ca1d6f62054f0b34615a6c99ea31232dd74a1e
Begnar001 <b.kasperski@gmail.com>
2014-05-12 17:53:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,27 @@ $zmienna =33 ;
 $zmienna_tekst = 'To jest jakis tekst. Poprawka';
 
 echo $zmienna;
+
+
+if (isset($_POST['password'])) {
+	$password = $_POST['password'];
+	$print_station_no = $_POST['print_station_no'];
+} else {
+	$password = "";
+	$print_station_no = "??";
+}
+//Setting Cockie to save Print station information. Need password and station number.
+if ($password == "1234") {
+	$print_station_id = $print_station_no;
+	setcookie("print_station_no_saved", $print_station_id, $expire);
+} else {
+	if (isset($_COOKIE["print_station_no_saved"])) {
+		$print_station_id = $_COOKIE["print_station_no_saved"];
+	} else {
+		$print_station_id = "??";
+	}
+}
+
+
+
 ?>
